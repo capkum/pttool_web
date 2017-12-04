325304f3f2a93a719bfd2cf13acea99bcd0cb9e4
Seoungjin Kim <capnaver@gmail.com>
2017-12-04 09:06:10 +0900

    change db

--- pttool_web/conf/settings.py
+++ pttool_web/conf/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'pttool.db3'),
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
